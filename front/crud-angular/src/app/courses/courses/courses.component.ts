a87eb4cf40d0edcbafaf0e692db039dbdc81a168
ItalloLinhares <45083049+ItalloLinhares@users.noreply.github.com>
2022-01-05 20:45:35 -0300

    Starting to add table

--- front/crud-angular/src/app/courses/courses/courses.component.ts
+++ front/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,29 @@
+import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+ @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
+  dataSource = DATA;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+
+
+const DATA: Course[] = [
+  {_id: '1', name: 'Hydrogen', category: 'a'},
+  {_id: '2', name: 'Helium', category: 'a'}
+];
+
+
